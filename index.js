574192ee22819e2251b3543125a0ed1bdfcddf35
Necron <139304358+4nec@users.noreply.github.com>
2024-03-27 01:53:17 +0700

    Update

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Listening to Necron: http://localhost:${port}`);
   console.log(`🔗 Powered By RTX`);
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to https://bit.ly/necronix","Watching Member Activity","Playing Faction Gathering"];
 
 
 let currentIndex = 0;
